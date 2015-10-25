23e2173d0a8c2e9cf9fdd809461cbe5accfe1e1a
vesters <v@re-cph.com>
2015-10-25 18:23:29 +0100

    Misc adjustments

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,8 @@ jQuery(window).load(function() {
 	jQuery(".status").fadeOut();
         // will fade out the whole DIV that covers the website.
 	jQuery(".preloader").delay(1000).fadeOut("slow");
+
+  jQuery("section.about").css('background', '#000');
 });
 
 /* =================================
